976da0cb6e2653438dfc4c006b34495b5a2a7915
Leonardo Omaña <leotangram@gmail.com>
2020-10-15 17:23:45 -0500

    Variables de entorno y carpeta pública

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express')
+require('dotenv').config()
 
 // Crear el servidor de express
 const app = express()
 
+// Public directory
+app.use(express.static('public'))
+
 // Routes
-app.get('/', (request, response) => {
-  response.json({
-    ok: true
-  })
-})
+// app.get('/', (request, response) => {
+//   response.json({
+//     ok: true
+//   })
+// })
 
 // Escuchar
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
