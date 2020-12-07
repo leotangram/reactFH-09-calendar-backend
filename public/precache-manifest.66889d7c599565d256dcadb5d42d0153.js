6517ffc513f69d11c1b77629d3138c251a4f265f
Leonardo Omaña <leotangram@gmail.com>
2020-12-07 16:16:28 -0500

    v1 CalendarApp

---
+++ public/precache-manifest.66889d7c599565d256dcadb5d42d0153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de97f1ddddec4373d2af113d9f3cbe18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d5f403e0ae92c065690",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "3c64b00267a7e209d94e",
+    "url": "/static/css/main.abfda070.chunk.css"
+  },
+  {
+    "revision": "9d5f403e0ae92c065690",
+    "url": "/static/js/2.da80b144.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.da80b144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c64b00267a7e209d94e",
+    "url": "/static/js/main.064d4b57.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  }
+]);
